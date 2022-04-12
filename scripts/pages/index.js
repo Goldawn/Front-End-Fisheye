52f6ac6d7503a7564b3cf6a823639c9680585138
Goldawn <heunow@gmail.com>
2022-04-12 15:31:43 +0200

    promise fix

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,51 +1,27 @@
+const defaultData = {
+    "name": "no data",
+    "id": -1,
+    "city": "",
+    "country": "",
+    "tagline": "",
+    "price": -1,
+    "portrait": "account.png"
+}
+
 async function getPhotographers() {
     const result = await fetch('data/photographers.json')
-    // const data = await result.json()
-    // console.log(data.photographers)
-    return result.json().photographers;
-        // .then(res => res.json())
-        // .then(res => {
-        //     console.log(res.photographers)
-        //     return res.photographers
-        // })
-        // .catch(err => console.log('erreur rencontrée', err))
-}   
+    const data = await result.json()
+    console.log(data.photographers)
+    return data.photographers? data.photographers : [defaultData];
+}
 
-// async function getPhotographers() {
-//     // Penser à remplacer par les données récupérées dans le json
-
-//     // const photographers = [
-//     //     {
-//     //         "name": "Ma data test",
-//     //         "id": 1,
-//     //         "city": "Paris",
-//     //         "country": "France",
-//     //         "tagline": "Ceci est ma data test",
-//     //         "price": 400,
-//     //         "portrait": "account.png"
-//     //     },
-//     //     {
-//     //         "name": "Autre data test",
-//     //         "id": 2,
-//     //         "city": "Londres",
-//     //         "country": "UK",
-//     //         "tagline": "Ceci est ma data test 2",
-//     //         "price": 500,
-//     //         "portrait": "account.png"
-//     //     },
-//     // ]
-//     let photographers;
-
-//     fetch("data/photographers.json")
-//     .then(res => res.json())
-//     .then(data => photographers = data.photographers)
-//     .then( () => {
-//         displayData(photographers);
-//     })
-//     .catch(err => console.log(err))
-//     // et bien retourner le tableau photographers seulement une fois
-//     // return (photographers)
+// async function getPhotographers () {
+//     return fetch('data/photographers.json')
+//         .then(res => res.json())
+//         .then(res => res.photographers)
+//         .catch(err => console.log('erreur rencontrée', err))
 // }
+
 
 async function displayData(photographers) {
     const photographersSection = document.querySelector(".photographer_section");
@@ -58,17 +34,14 @@ async function displayData(photographers) {
     });
 };
 
-// async function init() {
-//     // Récupère les datas des photographes
-//     const { photographers } = await getPhotographers();
-//     displayData(photographers);
-// };
-
-
 async function init() {
-    const photographers = await this.getPhotographers()
-    console.log(photographers)
+    const photographers = await getPhotographers()
+    // console.log(photographers)
     displayData(photographers);
 }
 
 init();
+
+module.exports = {
+    defaultData
+}
